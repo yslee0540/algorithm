7a9d5a76f9ef4c24247284c44963493c74d99c11
yslee0540 <124110627+yslee0540@users.noreply.github.com>
2023-08-29 15:33:32 +0000

    8/29

---
+++ 프로그래머스/lv2_4.py
@@ -0,0 +1,18 @@
+#타겟 넘버
+# from itertools import product
+# numbers = [1, 1, 1, 1, 1]
+# target = 3
+
+# l = [(x, -x) for x in numbers]
+# s = list(map(sum, product(*l)))
+# print(s)
+# print(s.count(target))
+
+#10진수 - k진수
+n = 437674
+k = 3
+rev_base = ''
+# while n > 0:
+#     rev_base += str(n % k)
+#     n //= k
+# print(rev_base[::-1])
