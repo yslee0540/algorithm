594c8231cd5d583cc9f27eab84492e0f9ef5b55b
yslee0540 <124110627+yslee0540@users.noreply.github.com>
2023-08-30 14:48:49 +0000

    8/30

--- 프로그래머스/lv2_4.py
+++ 프로그래머스/lv2_4.py
@@ -16,3 +16,30 @@ rev_base = ''
 #     rev_base += str(n % k)
 #     n //= k
 # print(rev_base[::-1])
+
+def convert(num, base):
+  arr = "0123456789ABCDEF"
+  q, r = divmod(num, base)
+  if q == 0:
+    return arr[r]
+  else:
+    return convert(q, base) + arr[r]
+
+# print(convert(15, 16))
+
+#더 맵게(힙)
+import heapq
+sc = [1, 2, 9, 3, 10, 12]
+K = 7
+heapq.heapify(sc)
+print(sc)
+
+answer = 0
+while sc[0] < K:
+    heapq.heappush(sc, heapq.heappop(sc) + heapq.heappop(sc) * 2)
+    answer += 1
+    
+    if len(sc) == 1 and sc[0] < K:
+      print(-1)
+      break
+print(answer)
