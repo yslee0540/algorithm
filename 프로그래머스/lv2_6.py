55240edad1e313f1c6b523bbed3b8d3a947f3272
yslee0540 <124110627+yslee0540@users.noreply.github.com>
2023-09-10 06:11:45 +0000

    9/8, 9/10

--- 프로그래머스/lv2_6.py
+++ 프로그래머스/lv2_6.py
@@ -21,3 +21,89 @@ def get_hanoi(n):
   return answer
 
 print(get_hanoi(3))
+
+#미로탈출
+# from collections import deque
+def bfs(start, end, maps):
+  dx = [-1, 1, 0, 0]
+  dy = [0, 0, -1, 1]
+  
+  n = len(maps)
+  m = len(maps[0])
+  visited = [[False] * m for _ in range(n)]
+  q = deque()
+  
+  for i in range(n):
+    for j in range(m):
+      if maps[i][j] == start:
+        q.append([i, j, 0])
+        visited[i][j] = True
+        break
+    if q:
+      break
+  
+  while q:
+    y, x, cnt = q.popleft()
+    if maps[y][x] == end:
+      return cnt
+    
+    for i in range(4):
+      ny = y + dy[i]
+      nx = x + dx[i]
+      
+      if 0 <= ny < n and 0 <= nx < m and maps[ny][nx] != 'X':
+        if not visited[ny][nx]:
+          q.append([ny, nx, cnt+1])
+          visited[ny][nx] = True
+  return -1
+
+def escape(maps):
+  path1 = bfs('S', 'L', maps)
+  path2 = bfs('L', 'E', maps)
+  
+  if path1 == -1 or path2 == -1:
+    return -1
+  return path1 + path2
+
+maps = ["SOOOL","XXXXO","OOOOO","OXXXX","OOOOE"]
+# print(escape(maps))
+
+#리코쳇 로봇
+from collections import deque
+def ricochet(board):
+  n = len(board)
+  m = len(board[0])
+  
+  dx = [-1, 1, 0, 0]
+  dy = [0, 0, -1, 1]
+  q = deque()
+  visited = [[False] * m for _ in range(n)]
+  
+  for i in range(n):
+    for j in range(m):
+      if board[i][j] == 'R':
+        q.append((i, j, 0))
+        visited[i][j] = True
+  
+  while q:
+    print(q)
+    x, y, cnt = q.popleft()
+    if board[x][y] == 'G':
+      return cnt
+    
+    for i in range(4):
+      scope = 1
+      while True:
+        nx = x + dx[i] * scope
+        ny = y + dy[i] * scope
+
+        if nx < 0 or nx >= n or ny < 0 or ny >= m or board[nx][ny] == 'D':
+          if not visited[nx-dx[i]][ny-dy[i]]:
+            visited[nx-dx[i]][ny-dy[i]] = True
+            q.append([nx-dx[i], ny-dy[i], cnt+1])
+          break
+        scope += 1
+  return -1
+
+board = ["...D..R", ".D.G...", "....D.D", "D....D.", "..D...."]
+print(ricochet(board))
